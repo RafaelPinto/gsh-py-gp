4e33ddaf4ce2962fcc5a7177b980f8fa94845aa8
RafaelPinto <rafaelpinto.ve@gmail.com>
2023-02-25 07:13:06 -0600

    Fix: Add docstring

--- src/data/quality.py
+++ src/data/quality.py
@@ -7,6 +7,19 @@ from src.data.utils import read_petrel_exported_pointset
 
 
 def check_grid_spacing(coordinate : pd.Series) -> np.ndarray:
+    """Print grid size value count and return unique coordinates.
+
+    Parameters
+    ----------
+    coordinate : pd.Series
+        The coordinate values, for example, the northing column.
+    
+    Returns
+    -------
+    unique_coordinate : np.ndarray
+        The unique coordinates sorted.
+    
+    """
     unique_coordinate = np.sort(coordinate.unique())
     grid_size_count = pd.Series(unique_coordinate).diff().value_counts()
     df = grid_size_count.to_frame(name="Count")
